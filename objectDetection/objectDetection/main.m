e7aa134b68a5a2da44d23d0c55b4e609a1f46a47
MattMckeller <mattmckeller@Matts-Mac.local>
2014-05-31 14:24:03 -0500

    first

---
+++ objectDetection/objectDetection/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  objectDetection
+//
+//  Created by Matt Mckeller on 5/29/14.
+//  Copyright (c) 2014 Matthew Mckeller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "objectDetectionAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([objectDetectionAppDelegate class]));
+    }
+}
---
+++ objectDetection/objectDetection/objectDetectionAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  objectDetectionAppDelegate.h
+//  objectDetection
+//
+//  Created by Matt Mckeller on 5/29/14.
+//  Copyright (c) 2014 Matthew Mckeller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface objectDetectionAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

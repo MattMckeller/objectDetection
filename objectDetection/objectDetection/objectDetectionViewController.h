bb924ea96496b86a385f9f530558a194a6ad5c73
MattMckeller <mattmckeller@Matts-Mac.local>
2014-05-31 14:29:31 -0500

    Init Commit Xcode

---
+++ objectDetection/objectDetection/objectDetectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  objectDetectionViewController.h
+//  objectDetection
+//
+//  Created by Matt Mckeller on 5/31/14.
+//  Copyright (c) 2014 Matthew Mckeller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface objectDetectionViewController : UIViewController
+
+@end
---
+++ objectDetection/objectDetection/objectDetectionViewController.m
@@ -0,0 +1,29 @@
+//
+//  objectDetectionViewController.m
+//  objectDetection
+//
+//  Created by Matt Mckeller on 5/31/14.
+//  Copyright (c) 2014 Matthew Mckeller. All rights reserved.
+//
+
+#import "objectDetectionViewController.h"
+
+@interface objectDetectionViewController ()
+
+@end
+
+@implementation objectDetectionViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
